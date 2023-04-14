0d4403506d6758dd5ac793e4dceb95ee237df8db
wei-rh <1759762906@qq.com>
2023-04-15 01:52:41 +0800

    实现了登录功能

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework import generics, viewsets
+from rest_framework import status
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import User
+from .middlewares import generate_custom_token
 
-# Create your views here.
+
+
+
+class Login(APIView):
+    def post(self, request):
+        try:
+            user = User.objects.get(phone_number=request.data["phone_number"],
+                                    password=request.data["password"])
+        except User.DoesNotExist:
+            return Response(data={"message":"error"}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            token = generate_custom_token(user, expiration_minutes=1)
+            return Response(data={"token":token, "message":"success"}, status=status.HTTP_200_OK)
+
+
+class Register(APIView):
+    def post(self, request):
+        us = UserSerializer(data=request.data)
+        if us.is_valid():
+            us.save()
+            return Response(data={"message": "success"}, status=status.HTTP_200_OK)
+        return Response(us.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GetList(APIView):
+    def get(self, request):
+        print(request.user, type(request.user))
+        s = UserSerializer(instance=User.objects.all(), many=True)
+        # 将所有用户序列化为一个列表
+        return Response(data=s.data, status=status.HTTP_200_OK)
+
